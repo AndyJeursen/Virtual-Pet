493be8435b89564fa3e12f40c740b0844a2ddb02
Andrea Jeursen <jeursen.andrea@gmail.com>
2022-10-11 20:12:30 +0200

    added js to hide and show name entry, added html for name entry, minor css changes

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+var nameInput = document.getElementById("name-input");
+var petName = document.getElementById("pet-name");
+var saveButton = document.getElementById("save-button");
+var editButton = document.getElementById("edit-button");
+var inputDiv = document.getElementById("input-div");
+
+saveButton.addEventListener("click",function(){
+    // get text from nameInput and set it on petName.
+    const newName = nameInput.value;
+    petName.innerHTML = newName;
+
+    inputDiv.style.height = "0px";
+    
+    nameInput.style.visibility = "hidden";
+    saveButton.style.visibility ="hidden";
+
+    petName.style.visibility = "visible";
+    editButton.style.visibility = "visible";
+    // Hide nameInput and saveButton. 
+    // Show petName and editButton
+})
+
+editButton.addEventListener("click", function (){
+    // make petName and editButton disappear. 
+    // Make nameInput and saveButton appear
+    petName.style.visibility = "hidden";
+    editButton.style.visibility = "hidden";
+
+    nameInput.style.visibility = "visible";
+    saveButton.style.visibility = "visible";
+    
+    inputDiv.style.height = null;
+})
+
+
 const decreasingConstant= 10; // default is 1
 const intervalConstant = 1; // default is 60
 
@@ -13,16 +48,19 @@ function feedPet(){
     }
 }
 
-const feedInterval = setInterval(function() {
+const feedIntervalId = setInterval(function() {
     decreaseFullness();
-}, 1000*intervalConstant);
+}, 600*intervalConstant);
 function decreaseFullness(){
     feedProgress.value = feedProgress.value -(1* decreasingConstant);
     if (feedProgress.value <= 0){
-        //alert("your pet has died");
+        deadPet();
+        clearInterval(feedIntervalId);
     }
 }
 
+// when each pet bar gets to zero, use if statement - stop whole app. where value of bar is. console log something. then show alert. 
+// create restart button
 
 
 
@@ -39,11 +77,15 @@ function cuddlePet(){
 
 }
 
-const cuddleInterval = setInterval(function() {
+const cuddleIntervalId = setInterval(function() {
     decreaseCuddleness();
 }, 1000*intervalConstant);
 function decreaseCuddleness(){
     cuddleProgress.value = cuddleProgress.value -(1* decreasingConstant);
+    if (cuddleProgress.value<=0){
+       deadPet();
+        clearInterval(cuddleIntervalId);
+    }
 }
 
 
@@ -62,11 +104,15 @@ function playPet(){
 
 }
 
-const playInterval = setInterval(function() {
+const playIntervalId = setInterval(function() {
     decreasePlayness();
-}, 1000*intervalConstant);
+}, 800*intervalConstant);
 function decreasePlayness(){
    playProgress.value = playProgress.value -(1* decreasingConstant);
+   if (playProgress.value<=0){
+    deadPet();
+    clearInterval(playIntervalId);
+   }
 }
 
 
@@ -85,14 +131,26 @@ function sleepPet(){
 
 }
 
-const sleepInterval = setInterval(function() {
+const sleepIntervalId = setInterval(function() {
     decreaseSleep();
-}, 1000*intervalConstant);
+}, 550*intervalConstant);
 function decreaseSleep(){
    sleepProgress.value = sleepProgress.value -(1* decreasingConstant);
+   if (sleepProgress.value<=0){
+    deadPet();
+    clearInterval(sleepIntervalId);
+   }
 }
 
-// create save pet function
-// kill pet
-// name pet
-// pet image to change
+var isDead= false;
+
+function deadPet(){
+    if (isDead){
+        
+    }
+    console.log("dead pet function");
+    isDead = true;
+}
+
+// create save pet function - kill pet - pet image to change
+// create function that runs when pet dies, run this function in each if statement where the progress bar is zero
